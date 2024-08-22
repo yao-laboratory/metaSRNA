10cc143478d68c7afb526f7c92892abf32c62cac
Eilenechou <mzhou10@huskers.unl.edu>
2024-08-22 14:05:39 -0500

    encapsulate functions and varibles for this shell

--- release_version/code/shell/integration.sh
+++ release_version/code/shell/integration.sh
@@ -1,25 +1,109 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Load required modules
 module load blast/2.14
 module load biodata/1.0
 
-code_path_for_clean=$1
-code_path_after_clean=$2
-input_fastq=$3
-mapping_species=$4
-mapping_mirna=$5
-results=$6
-
-python3 $code_path/filter_blast_results_length.py -input_file $mapping_species -start 18 -end 40 -output_folder $results
-python3 ${code_path_for_clean}/process_sequence_length.py process_length -input $input_fastq -filter_min_length 18 -filter_max_length 40 -output_folder $results/middle_results
-seqtk seq -a $results/middle_results/final_seq_18_to_40.fastq  > $results/middle_results/final_seq_18_to_40.fasta
-
-## filter species mapping file with new reads length requirements
-python3 ${code_path_after_clean}/filter_blast_result_length.py -input_mapping_file $mapping_species/blast_score_filter.txt -input_filtered_clean_fasta $results/middle_results/final_seq_18_to_40.fasta -output_file ${results}/blast_score_final_filter.txt
-### produce mapping species file and mapping mirna file venn diagram results
-python3 ${code_path_after_clean}/mirna_and_species_blast_overlap.py -species_mapping_file ${results}/blast_score_final_filter.txt -hairpin_folder $mapping_mirna -overlap_folder $results
-
-# ## nead filter filtering reads only match species part, this is an input for predict step
-python3 ${code_path_after_clean}/prepare_reads_file.py -input_reads $results/middle_results/final_seq_18_to_40.fasta -csv_file $mapping_species/blast_score_filter.txt -output_file $results/final_species.fasta
+# Global variables
+CODE_PATH_FOR_CLEAN=$1
+CODE_PATH_AFTER_CLEAN=$2
+INPUT_FASTQ=$3
+MAPPING_SPECIES=$4
+MAPPING_MIRNA=$5
+UMI_FILE=$6
+RESULTS=$7
 
 
+
+# Function to process sequence length
+filter_sequence_length() {
+    local input_fastq=$1
+    local output_folder=$2
+    
+    python3 "${CODE_PATH_FOR_CLEAN}/filter_sequence_length.py" process_length \
+        -input "${input_fastq}" \
+        -filter_min_length 18 \
+        -filter_max_length 40 \
+        -output_folder "${output_folder}"
+}
+
+# Function to convert fastq to fasta
+convert_fastq_to_fasta() {
+    local input_fastq=$1
+    local output_fasta=$2
+
+    seqtk seq -a "${input_fastq}" > "${output_fasta}"
+}
+
+# Function to prepare miRDeep2 input
+prepare_mirdeep2_input() {
+    local input_fasta=$1
+    local csv_file=$2
+    local output_file=$3
+    
+    python3 "${CODE_PATH_AFTER_CLEAN}/prepare_reads_file.py" \
+        -input_reads "${input_fasta}" \
+        -csv_file "${csv_file}" \
+        -output_file "${output_file}"
+}
+
+# Function to prepare linearfold input
+prepare_linearfold_input() {
+    local code_path_after_clean=$1
+    local mirdeep2_input_fasta=$2
+    local umi_file=$3
+    local mapping_species=$4
+    local results=$5
+
+    # Filter redundant sequences and prepare linearfold input
+    python3 "${code_path_after_clean}/filtering_redundant_sequences.py" \
+        -seq_reads "${mirdeep2_input_fasta}" \
+        -umi_reads "${umi_file}" \
+        -output_fasta "${results}/linearfold_input.fasta" \
+        -output_csv "${results}/redundant_sequences_information.csv"
+
+    # Filter species mapping file with new reads
+    python3 "${code_path_after_clean}/filter_mapping_result.py" \
+        -input_mapping_file "${mapping_species}/blast_score_filter.txt" \
+        -input_filtered_clean_fasta "${results}/linearfold_input.fasta" \
+        -output_file "${results}/blast_score_linearfold_filter.txt"
+}
+
+
+# Function to produce overlap analysis
+produce_overlap_analysis() {
+    local species_mapping_file=$1
+    local hairpin_folder=$2
+    local overlap_file=$3
+    
+    python3 "${CODE_PATH_AFTER_CLEAN}/mirna_and_species_blast_overlap.py" \
+        -species_mapping_file "${species_mapping_file}" \
+        -hairpin_folder "${hairpin_folder}" \
+        -overlap_file "${overlap_file}"
+}
+
+main() {
+    local middle_results="${RESULTS}/middle_results"
+    # local umi_file="path_to_umi_file"  # Define the path to the umi_file
+
+    # Process sequence length and convert to fasta
+    filter_sequence_length "${INPUT_FASTQ}" "${middle_results}"
+    convert_fastq_to_fasta "${middle_results}/final_seq_18_to_40.fastq" "${middle_results}/final_seq_18_to_40.fasta"
+
+    # Prepare miRDeep2 input
+    prepare_mirdeep2_input "${middle_results}/final_seq_18_to_40.fasta" "${MAPPING_SPECIES}/blast_score_filter.txt" "${RESULTS}/mirdeep2_input.fasta"
+
+    # Prepare Linearfold input
+    prepare_linearfold_input "$CODE_PATH_AFTER_CLEAN" "$RESULTS/mirdeep2_input.fasta" "$UMI_FILE" "$MAPPING_SPECIES" "$RESULTS"
+
+    # Produce overlap analysis
+    produce_overlap_analysis "${MAPPING_SPECIES}/blast_score_filter.txt" "${MAPPING_MIRNA}" "${RESULTS}/mirna_and_top_species_analysis.csv"
+    ###still have problems, mirna mapping sequence part not unique
+    # produce_overlap_analysis "${RESULTS}/blast_score_linearfold_filter.txt" "${MAPPING_MIRNA}" "${RESULTS}/mirna_and_top_species_analysis_unique_sequences.csv"
+}
+
+main "$@"
