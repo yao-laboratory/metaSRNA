d38d9a28e92fa67cb6b3f4d5751903efe607d6b1
Eilenechou <mzhou10@huskers.unl.edu>
2024-10-01 14:50:31 -0500

    optimize species detection shell

--- release_version/code/shell/species_detection.sh
+++ release_version/code/shell/species_detection.sh
@@ -1,48 +1,105 @@
 #!/bin/bash
+
 # Exit immediately if any command fails
 set -e
-
-refprok_database=$1
-code_path=$2
-top_num=$3
-clean_fasta=$4
-results=$5
-temp_folder=$results/middle_results
-blast_refprok_mapping_fna=$temp_folder/ncbi_dataset_fna/ncbi_dataset/data
-
-echo "$refprok_database"
-echo "$code_path"
-echo "$clean_fasta"
-echo "$results"
-
+set -o pipefail
 module load blast/2.14
 module load biodata/1.0
+# Global variables for input arguments
+REFPROK_DATABASE="$1"
+CODE_PATH="$2"
+TOP_NUM="$3"
+CLEAN_FASTA="$4"
+RESULTS="$5"
 
-##start, 1st step
-# blastn -query $clean_fasta -db ${refprok_database}/ref_prok_rep_genomes -out $results/middle_results/blastn_refprok_evalue10.txt -num_threads 4 -evalue 10 -outfmt "6 qseqid sacc sstart send evalue bitscore qcovhsp pident sseqid staxids sscinames sblastnames" -max_target_seqs 5 -max_hsps 3 -task "blastn"
+TEMP_FOLDER="$RESULTS/middle_results"
+mkdir -p "$TEMP_FOLDER"mkdir -p "$TEMP_FOLDER"
+BLAST_REFPROK_MAPPING_FNA="$TEMP_FOLDER/ncbi_dataset_fna/ncbi_dataset/data"
 
-# wc -l $results/middle_results/blastn_refprok_evalue10.txt
-# awk -F'\t' '{print $1}' $results/middle_results/blastn_refprok_evalue10.txt | sort -n | uniq -c | wc -l
+printf "Refprok Database: %s\nCode Path: %s\nClean Fasta: %s\nResults: %s\n" \
+  "$REFPROK_DATABASE" "$CODE_PATH" "$CLEAN_FASTA" "$RESULTS" >&2
 
-###2nd step
-num=$(wc -l $clean_fasta |awk '{print $1/2}')
-echo $num
-python3 ${code_path}/blast_species_classification.py -input_folder $results/middle_results -total_sequence_number $num  -output_folder $results
+# run blastn
+run_blastn() {
+  local blast_output="$TEMP_FOLDER/blastn_refprok_evalue10.txt"
+  blastn -query "$CLEAN_FASTA" -db "${REFPROK_DATABASE}/ref_prok_rep_genomes" \
+    -out "$blast_output" -num_threads 4 -evalue 10 \
+    -outfmt "6 qseqid sacc sstart send evalue bitscore qcovhsp pident sseqid staxids sscinames sblastnames" \
+    -max_target_seqs 5 -max_hsps 3 -task "blastn"
+  
+  if [[ ! -s "$blast_output" ]]; then
+    printf "BLASTn output is empty. Exiting.\n" >&2
+    return 1
+  fi
 
-###3rd step
-python3 ${code_path}/union_top_species_classification.py -input_folder $results/middle_results -top_count $top_num -output_folder $results
+  printf "BLASTn complete. Output written to %s\n" "$blast_output"
+}
 
-###4th step: this step need user add all not found gcf_numbers to csv
-##hand made step
+# Function to perform line count and unique sequence extraction
+process_blast_output() {
+  local blast_output="$TEMP_FOLDER/blastn_refprok_evalue10.txt"
+  local unique_seq_count
 
-###optional 5th step
-gcf_numbers=$(awk -F'\t' 'NR > 1 && $2 {print $2}' $results/mapping.csv | tr '\n' ' ')
-echo $gcf_numbers
+  printf "Processing BLAST output file: %s\n" "$blast_output"
+  wc -l "$blast_output"
+  unique_seq_count=$(awk -F'\t' '{print $1}' "$blast_output" | sort -n | uniq -c | wc -l)
+  
+  printf "Unique sequence count: %s\n" "$unique_seq_count"
+}
 
-curl -o $temp_folder/datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets'
-chmod 777 $temp_folder/datasets
-${temp_folder}/datasets download genome accession ${gcf_numbers} --include genome --filename $temp_folder/ncbi_dataset_fna.zip
-# ${temp_folder}/datasets download genome accession ${gcf_numbers} --include genome --format gtf --filename $temp_folder/ncbi_dataset_gtf.zip
-unzip -o $temp_folder/ncbi_dataset_fna.zip -d $temp_folder/ncbi_dataset_fna
-find $temp_folder/ncbi_dataset_fna/ncbi_dataset/data -type f -name "*.fna" -exec cat {} + > $results/combined_gcf.fna
+# Function to run species classification scripts
+run_species_classification() {
+  local num_sequences
+  local species_output
 
+  num_sequences=$(wc -l $CLEAN_FASTA |awk '{print $1/2}')
+  printf "Total sequences: %s\n" "$num_sequences"
+
+  python3 "${CODE_PATH}/blast_species_classification.py" \
+    -input_folder "$TEMP_FOLDER" \
+    -total_sequence_number "$num_sequences" \
+    -output_folder "$RESULTS"
+
+  python3 "${CODE_PATH}/union_top_species_classification.py" \
+    -input_folder "$TEMP_FOLDER" \
+    -top_count "$TOP_NUM" \
+    -output_folder "$RESULTS"
+}
+
+# Function to download genomes using accession numbers from mapping.csv
+download_genomes() {
+  local gcf_numbers
+
+  gcf_numbers=$(awk -F'\t' 'NR > 1 && $2 {print $2}' "$RESULTS/mapping.csv" | tr '\n' ' ')
+  
+  if [[ -z "$gcf_numbers" ]]; then
+    printf "No GCF numbers found in mapping.csv. Exiting.\n" >&2
+    return 1
+  fi
+  
+  printf "GCF numbers: %s\n" "$gcf_numbers"
+
+  # Ensure dataset CLI is downloaded
+  if [[ ! -f "$TEMP_FOLDER/datasets" ]]; then
+    curl -o "$TEMP_FOLDER/datasets" 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets'
+    chmod 777 "$TEMP_FOLDER/datasets"
+  fi
+
+  "${TEMP_FOLDER}/datasets" download genome accession ${gcf_numbers} \
+    --include genome --filename "$TEMP_FOLDER/ncbi_dataset_fna.zip"
+
+  unzip -o "$TEMP_FOLDER/ncbi_dataset_fna.zip" -d "$TEMP_FOLDER/ncbi_dataset_fna"
+
+  find "$BLAST_REFPROK_MAPPING_FNA" -type f -name "*.fna" -exec cat {} + > "$RESULTS/combined_gcf.fna"
+}
+
+# Main function to coordinate the script flow
+main() {
+  run_blastn
+  process_blast_output
+  run_species_classification
+  download_genomes
+}
+
+# Execute main function
+main "$@"
