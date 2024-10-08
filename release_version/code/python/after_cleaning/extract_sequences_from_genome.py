1beb8298fabd0dcc8d5e4eb41f509ebf89c9586d
Eilenechou <mzhou10@huskers.unl.edu>
2024-10-08 12:44:16 -0500

    let this code support multiple seqnames in fna

--- release_version/code/python/after_cleaning/extract_sequences_from_genome.py
+++ release_version/code/python/after_cleaning/extract_sequences_from_genome.py
@@ -5,6 +5,7 @@ from Bio.SeqRecord import SeqRecord
 
 EXTRA_LEFT_GAP = 40
 EXTRA_RIGHT_GAP = 40
+START_BORDER = 1
 
 def extract_sequences_from_genome(input_blast_result, input_genome, output_fasta):
     # input_blast_result = '../../../mytuber/results/SRR20723831/blast_result/blastn_mapping_SRR20723831_test.txt'
@@ -15,33 +16,54 @@ def extract_sequences_from_genome(input_blast_result, input_genome, output_fasta
 
     # input_genome = '../../input_data/database/blast_mytuber_fna/test.fasta'
     seq = Seq("")
-    #seq start index is 0
+    sequence_dict = []
+    # #seq start index is 0
+    # for record in SeqIO.parse(input_genome, "fasta"):
+    #     # Create a Seq object from the sequence data
+    #     seq = Seq(record.seq)
+    #     # print(input_genome, len(seq))
     #this following fasta is whole genome fasta file
-    for record in SeqIO.parse(input_genome, "fasta"):
-        # Create a Seq object from the sequence data
-        seq = Seq(record.seq)
-        # print(input_genome, len(seq))
-    start_border = 1
-    end_border = len(seq)
+    sequence_dict = SeqIO.to_dict(SeqIO.parse(input_genome, "fasta"))
+    # print("sequence_dict:",sequence_dict)
+
     print("part1 done")
     records = []
     with open(output_fasta, "w") as output_file:
         for index, row in df_blast_score.iterrows():
             seq_id = row['qseqid']
+            sacc = row['sacc']
+            # print("sacc", sacc)
             start = int(row['sstart'])
             end = int(row['send'])
-            
+            end_border = len(sequence_dict[sacc])
             if start < end:
                 #number border protection
-                df_start_pos = (start - EXTRA_LEFT_GAP) if (start - EXTRA_LEFT_GAP) >= start_border else start_border
+                df_start_pos = (start - EXTRA_LEFT_GAP) if (start - EXTRA_LEFT_GAP) >= START_BORDER else START_BORDER
                 df_end_pos = (end + EXTRA_RIGHT_GAP) if (end + EXTRA_RIGHT_GAP) <= end_border else end_border
                 # records.append({'seq_id': seq_id, 'seq': str(seq[df_start_pos: df_end_pos])})
-                SeqIO.write(SeqRecord(Seq(str(seq[df_start_pos: df_end_pos + 1])), id=str(seq_id), description="index_"+str(index)), output_file, "fasta")
+                extend_seq = sequence_dict[sacc].seq[df_start_pos: df_end_pos + 1]
+                # print("+")
+                # print("extend_whole_seq", sequence_dict[sacc].seq[0:2])
+                # print("original start", start)
+                # print("original end", end)
+                # print("start:",df_start_pos)
+                # print("end:",df_end_pos + 1)
+                # print("extend_seq", extend_seq)
+                SeqIO.write(SeqRecord(extend_seq, id=str(seq_id), description="index_"+str(index)), output_file, "fasta")
                 # print(records[-1],df_start_pos,df_end_pos)
             else:
-                df_start_pos = (end - EXTRA_LEFT_GAP) if (end - EXTRA_LEFT_GAP) >= start_border else start_border
+
+                df_start_pos = (end - EXTRA_LEFT_GAP) if (end - EXTRA_LEFT_GAP) >= START_BORDER else START_BORDER
                 df_end_pos = (start + EXTRA_RIGHT_GAP) if (start + EXTRA_RIGHT_GAP) <= end_border else end_border
-                SeqIO.write(SeqRecord(Seq(str(seq[df_start_pos: df_end_pos + 1].reverse_complement())), id=str(seq_id), description="index_"+str(index)), output_file, "fasta")
+                extend_seq = sequence_dict[sacc].seq[df_start_pos: df_end_pos + 1].reverse_complement()
+                # print("-")
+                # print("extend_whole_seq", sequence_dict[sacc].seq[0:2])
+                # print("original start", start)
+                # print("original end", end)
+                # print("start:",df_start_pos)
+                # print("end:",df_end_pos + 1)
+                # print("extend_seq", extend_seq)
+                SeqIO.write(SeqRecord(extend_seq, id=str(seq_id), description="index_"+str(index)), output_file, "fasta")
                 #records.append({'seq_id': seq_id, 'seq': str(seq[df_start_pos: df_end_pos].reverse_complement())})
                 # print(records[-1],df_end_pos,df_start_pos)
     # Write to a FASTA file
